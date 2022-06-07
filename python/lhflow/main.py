1a9783c99a9bdbbf15bc5226d292627dbe6629be
Ross Sharma <ross@orderboard.ai>
2022-06-06 18:38:22 -0700

    wip

--- python/lhflow/main.py
+++ python/lhflow/main.py
@@ -3,13 +3,11 @@ from pathlib import Path
 import pickle
 
 taskfile = Path("/home/rsh/.tasks")
-
-
 Task = str
-TaskList = list[Task]
+Tasks = list[Task]
 
 
-def load_tasks() -> TaskList:
+def load_tasks() -> Tasks:
     if not taskfile.is_file():
         save_tasks([])
 
@@ -17,19 +15,9 @@ def load_tasks() -> TaskList:
         return pickle.load(f)
 
 
-def save_tasks(tasks: TaskList):
+def save_tasks(tasks: Tasks):
     with open(taskfile, "wb") as f:
         pickle.dump(tasks, f)
-
-
-def list_tasks(tasks: TaskList):
-    for count, task in enumerate(tasks):
-        print(f"{count}. {task}")
-
-
-def print_active_task(tasks: TaskList):
-    task = get_active_task(tasks)
-    print(f"The active task is now '{task}'")
 
 
 def get_active_task(tasks):
@@ -38,10 +26,23 @@ def get_active_task(tasks):
     return None
 
 
+def list_tasks(tasks: Tasks):
+    for count, task in enumerate(tasks):
+        print(f"{count}. {task}")
+
+
+def print_active_task(tasks: Tasks):
+    task = get_active_task(tasks)
+    print(f"The active task is now '{task}'")
+
+def ensure_task_does_not_exist(task, tasks):
+    if task in tasks:
+        raise Exception('Task already exists')
+
 def main(args: list[str]) -> int:
-    exit_code = 0
     tasks = load_tasks()
     action = args[1]
+    active_task = get_active_task(tasks)
 
     if action == "list":
         list_tasks(tasks)
@@ -49,26 +50,31 @@ def main(args: list[str]) -> int:
     elif action == "pop":
         if not tasks:
             print("Error: There are no tasks in the stack.")
-            exit_code = 1
         else:
             tasks.pop(0)
 
     elif action == "push":
         task = " ".join(args[2:])
+        ensure_task_does_not_exist(task, tasks)
         tasks = [task] + tasks
-        print(f"The active task is now '{task}'")
+
+    elif action == "append":
+        task = " ".join(args[2:])
+        ensure_task_does_not_exist(task, tasks)
+        tasks.append(task)
 
     elif action == "reset":
         tasks = []
+
     else:
         print(f"Unrecognized action: {action}")
-        exit_code = 1
+
+    task = get_active_task(tasks)
+    if task != active_task:
+        print(f"The active task is now: {task}")
 
     save_tasks(tasks)
-    return exit_code
 
 
 if __name__ == "__main__":
-    exit_code = main(sys.argv)
-    print(f"Exiting with code: {exit_code}")
-    sys.exit(exit_code)
+    main(sys.argv)
