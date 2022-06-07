0f901238752b6ec4d009d5c4eb884b512edeb5bf
Ross Sharma <ross@orderboard.ai>
2022-06-07 14:40:43 -0700

    green

--- python/tester.py
+++ python/tester.py
@@ -1,27 +1,25 @@
 from dataclasses import dataclass
 import os
 import subprocess
-from  assertpy import assert_that
-from obdev_utils.common import run_command, get_logger
-
+from assertpy import assert_that
+from obdev_utils.common import run_args, get_logger, Args
 
 
 @dataclass
 class CliTest:
-    args: list[str]
+    args: Args
     stdout: str
     stderr: str
     exit_code: int
 
+
 def run_test(test: CliTest):
-    result = run_command(test.args)
+    result = run_args(test.args)
     assert_that(test.stdout.strip()).is_equal_to(result.stdout.strip())
     # assert test.stderr == result.stderr.decode()
     # assert test.exit_code == result.returncode
 
-if __name__ == '__main__':
-    test = CliTest(
-            "echo 1".split(), "1", "", 0
-            )
 
-    run_test(test)
+if __name__ == "__main__":
+    ct = CliTest("echo 1".split(), "1", "", 0)
+    run_test(ct)
